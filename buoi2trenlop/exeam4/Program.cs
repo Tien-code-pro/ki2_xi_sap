792a6deccf58d2952d6ab97209f8136bd69beccd
Tien-code <80895788+Tien-code-pro@users.noreply.github.com>
2021-06-30 17:31:26 +0700

    task5 C#

---
+++ buoi2trenlop/exeam4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exeam4
+{
+    
+    class TestProgram
+    {
+        void Count(int boys, int girls)
+        {
+            Console.WriteLine(boys + girls);
+        }
+        static void Main(string[] args)
+        {
+            TestProgram objTest = new TestProgram();
+            objTest.Count(boys: 16, girls: 24);
+        }
+    }
+}
---
+++ buoi2trenlop/exeam5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exeam5
+{
+    using System;
+    class OptionalParameterExample
+    {
+        void printMessage(String message = "Hello user!")
+        {
+            Console.WriteLine("{0}", message);
+        }
+        static void Main(string[] args)
+        {
+            OptionalParameterExample opExample = new OptionalParameterExample();
+            opExample.printMessage("Welcome User!");
+            opExample.printMessage();
+        }
+    }
+}
