f0bb67cef33bba8e6476b458a8feb3957ac91342
Tien-code <80895788+Tien-code-pro@users.noreply.github.com>
2021-07-12 13:44:06 +0700

    lap4

---
+++ buoi4trenlop/Code Snippet 1/Code Snippet 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Code_Snippet_1
+{
+    abstract class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Every animal eats food in order to survive");
+        }
+        public abstract void AnimalSound();
+    }
+    class Lion : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Lion roars");
+        }
+        static void Main(string[] args)
+        {
+            Lion objLion = new Lion();
+            objLion.AnimalSound();
+            objLion.Eat();
+        }
+    }
+}
