00e7a9eaa6e93dc5d6529ae2bfb6f4898dd3e753
lương Tiến <80895788+Tien-code-pro@users.noreply.github.com>
2021-07-12 18:07:07 +0700

    task8

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Self_Assessnet_lAB
+{
+    abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string phone;
+
+    }
+}
