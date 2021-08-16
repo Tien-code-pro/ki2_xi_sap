6ce00cab9c64214cb1fded1574cfd12ab1283bd5
Tien-code <80895788+Tien-code-pro@users.noreply.github.com>
2021-08-16 14:26:15 +0700

    learn laravel

---
+++ Codelean_eCommerce/app/Http/Controllers/Front/ShopController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductComment;
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    //
+    public function show($id){
+        $product = Product::findOrFail($id);
+
+        $avgRating =0;
+        $sumRating = array_sum(array_column($product->productComments->toArray(),'rating'));
+        $countRating = count($product->productComments);
+        if ($countRating !=0 ){
+            $avgRating =$sumRating/$countRating;
+        }
+        return view('front.shop.show',compact('product','avgRating'));
+    }
+    public function postComment(Request $request){
+        ProductComment::create( $request->all() );
+        return redirect()->back();
+    }
+
+}
